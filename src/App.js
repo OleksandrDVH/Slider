083b3d4f43faf481a86b03974f3ccc4d102b7ead
oleksandr <dovhoteles.oleksandr@gmail.com>
2023-08-21 00:00:07 +0300

    used HOC

--- src/App.js
+++ src/App.js
@@ -1,152 +1,102 @@
-import {Component, useState, useEffect, useCallback, useMemo} from 'react';
+import {useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-// class Slider extends Component {
+const withSlider = (BaseComponent, getData) => {
+    return (props) => {
+        const [slide, setSlide] = useState(0);
+        const [autoplay, setAutoplay] = useState(false)
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
-//         }
-//     }
+        useEffect(() => {
+            setSlide(getData());
+        }, [])
 
-//     componentDidMount() {
-//         document.title = `Slide: ${this.state.slide}`;
-//     }
-
-//     componentDidUpdate() {
-//         document.title = `Slide: ${this.state.slide}`;
-//     }
-
-//     changeSlide = (i) => {
-//         this.setState(({slide}) => ({
-//             slide: slide + i
-//         }))
-//     }
-
-//     toggleAutoplay = () => {
-//         this.setState(({autoplay}) => ({
-//             autoplay: !autoplay
-//         }))
-//     }
-
-//     render() {
-//         return (
-//             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                     </div>
-//                 </div>
-//             </Container>
-//         )
-//     }
-// }
-
-
-const countTotal = (num) => {
-    console.log('counting...');
-    return num + 10;
+        function changeSlide(i) {
+            setSlide(slide => slide + i);
+        }
+        
+        return <BaseComponent 
+                {...props}
+                slide={slide} 
+                autoplay={autoplay} 
+                changeSlide={changeSlide}
+                setAutoplay={setAutoplay}/>
+    }
 }
 
-const Slider = (props) => {
+const getDataFromFirstFetch = () => {return 10};
+const getDataFromSecondFetch = () => {return 20};
 
-    const [slide, setSlide] = useState(0);
-    const [autoplay, setAutoPlay] = useState(false);
-
-    const getSomeImages = useCallback(() => {
-        console.log('fetching')
-        return [
-            "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
-            "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
-        ]
-    }, [slide]);
-
-
-    function changeSlide(i) {
-        setSlide(slide => slide + i);
-    }
-
-    function toggleAutoplay() {
-        setAutoPlay(autoplay => !autoplay)
-    }
-
-    const total = useMemo(() => {
-        return countTotal(slide);
-    }, [slide]);
-
-    const style = useMemo(() => ({
-        color: slide > 4 ? 'red' : 'black'
-    }), [slide])
-
-    useEffect(() => {
-        console.log('styles!');
-    }, [style])
-
-
+const SliderFirst = (props) => {
     return (
         <Container>
             <div className="slider w-50 m-auto">
-
-                <Slide getSomeImages={getSomeImages}/>
-
-                <div className="text-center mt-5">Active slide {slide} <br/>
-                {autoplay ? 'auto' : null}
-                </div>
-                <div style={style} className="text-center mt-5">Total slides: {total}
-                </div>
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {props.slide}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}>-1</button>
+                        onClick={() => props.changeSlide(-1)}>-1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}>+1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={toggleAutoplay}>toggle autoplay</button>
+                        onClick={() => props.changeSlide(1)}>+1</button>
                 </div>
             </div>
         </Container>
     )
 }
 
-const Slide = ({getSomeImages}) => {
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        setImages(getSomeImages())
-    }, [getSomeImages])
+const SliderSecond = (props) => {
     return (
-        <>
-            {images.map((url, i) => <img key={i} className="d-block w-100" src={url} alt="slide" />)}
-        </>
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {props.slide} <br/>{props.autoplay ? 'auto' : null} </div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => props.setAutoplay(autoplay => !props.autoplay)}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
     )
 }
 
+const SliderWithFirstFetch = withSlider(SliderFirst, getDataFromFirstFetch);
+const SliderWithSecondFetch = withSlider(SliderSecond, getDataFromSecondFetch);
 
+
+const withLogger = WrappedComponent => props => {
+    useEffect(() => {
+        console.log('first render');
+    }, [])
+
+    return <WrappedComponent {...props}/>
+}
+
+
+const Hello = () => {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+const HelloWithLogger = withLogger(Hello);
+ 
 function App() {
-    const [slider, setSlider] = useState(true);
-
-
-  return (
+    return (
         <>
-            <button onClick={() => setSlider(false)}>Click</button>
-            {slider ? <Slider/> : null}
+            <HelloWithLogger/>
+           <SliderWithFirstFetch/>
+           <SliderWithSecondFetch/>
         </>
-  );
+    );
 }
 
 export default App;
